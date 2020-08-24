3f5c9cf03cc1ccde46f55c7a8c755fefb2e36723
mehrdadkarami <mehrdad.allahkarami@syncier.com>
2020-08-24 12:24:15 +0200

    footer

---
+++ src/assets/js/apps/blog/handlers/AboutHandler.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import About from "../components/About";
+
+export default class AboutHandler extends React.Component {
+
+    render() {
+        return <div className="content">
+            <section>
+                <About link="/data/footer/about.md"/>
+            </section>
+        </div>;
+    }
+}
---
+++ src/assets/js/apps/blog/handlers/FooterHandler.jsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import {Link} from 'react-router';
+
+export default class FooterHandler extends React.Component {
+
+    render() {
+        return <footer className="footer-distributed">
+
+            <div className="footer-left">
+
+                <h3>Online<span>Java</span>Class</h3>
+
+                <p className="footer-company-name">&copy;Copyright 2015-2020 All rights reserved.<br/> This blog is
+                    fully run by myself and the powerful React framework</p>
+            </div>
+
+            <div className="footer-center">
+
+                <div>
+                    <Link to="about" activeClassName="active">
+                        <i className="fap fap-avatar"/>
+                        About Me
+                    </Link>
+                </div>
+                <div>
+                    <Link to="privacy" activeClassName="active">
+                        <i className="fap fap-phone"/>
+                        <p>Privacy Policy</p>
+                    </Link>
+                </div>
+                <div>
+                    <Link to="terms" activeClassName="active">
+                        <i className="fap fap-phone"/>
+                        <p>Terms of Use</p>
+                    </Link>
+                </div>
+                <div>
+                    <i className="fap fap-envelope"/>
+                    <p><a href="mailto:support@company.com">contact@onlinejavaclass.com</a></p>
+                </div>
+
+            </div>
+
+            <div className="footer-right">
+
+                <p className="footer-company-about">
+                    <span>About Online Java Class</span>
+                    Online Java Class has different classes in Java for beginners and professionals.
+                </p>
+
+                <div className="footer-icons">
+                    <a href="#"><i className="fap fap-linkedin"/></a>
+                    <a href="#"><i className="fap fap-github"/></a>
+                </div>
+
+            </div>
+
+        </footer>;
+    }
+}
