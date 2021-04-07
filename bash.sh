43ee0d63d1672eb177ff2c168ad93773e7ba656b
Mehrdad Karami <mehrdadallahkarami@gmail.com>
2021-04-07 16:13:31 +0200

    updated files structure

---
+++ bash.sh
@@ -0,0 +1,15 @@
+cat <<EOF
+/.github/
+/.idea/
+/deployment/
+/gulp-plugins/
+/node_modules/
+/src/
+/.eslintrc
+/.gitignore
+/gulpfile.js
+/package.json
+/package-lock.json
+/README.md
+/webpack.config.js
+EOF
