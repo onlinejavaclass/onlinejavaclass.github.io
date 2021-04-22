695eb7d2b5b20439ae3befc206ac7ca1901032d0
onlinejavaclass <runner@fv-az167-735.21ktoceh2wee5fewwtpg1zd01g.bx.internal.cloudapp.net>
2021-04-22 22:32:16 +0000

    deployment done

--- src/assets/js/apps/blog/utils/Array.js
+++
@@ -1,5 +0,0 @@
-/*eslint-disable no-extend-native */
-  Array.prototype.flatMap = function(lambda) {
-      return Array.prototype.concat.apply([], this.map(lambda));
-  };
-/*eslint-enable no-extend-native */
